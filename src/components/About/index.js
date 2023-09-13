5433fd0913bc742d98e90019511212c0326aa93c
sushmaprocorp <sushmarao@procorp.ltd>
2023-09-13 12:30:52 +0530

    first commit

---
+++ src/components/About/index.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import './index.css';
+
+
+function index() {
+  return (
+    <div>
+    <div>
+    <h2 className='heading'>ProCorp</h2>
+    <p className='para'>Welcome to ProCrop,where innovation meets passion. ProCorp (PROPANION INDIA PVT LTD) is a DPIIT recognised Startup based in Hyderabad, India. Since our inception, <br/>
+    we've been on a journey of growth, learning, and relentless pursuit of excellence. At the core ProCorp is a technology services and consulting company focused on buliding innovative  <br/>
+    solutions that address clients' most complex digital tansformation needs.</p>
+    <p className='para'>We leverage our holistic portfolio of capabilities in consulting, design, engineering, operations, and emerging technologies to help clients realise their boldest ambitions and build  <br/>
+    future-ready, sustainable businesses.We take pride in our diverse and talented team that brings a wealth of expertise to the table. Together, we're united by a common goal  <br/>
+    giving the best services to our clients</p>
+    <img src='https://res.cloudinary.com/dh0odgkdj/image/upload/v1693214216/Image_2_lraryx.png'
+     alt="image" className='image'/>
+     <img src="https://res.cloudinary.com/dh0odgkdj/image/upload/v1693214261/Image_1_y6if2o.png"
+     alt="image" className='image'/>
+     <div className='do'>
+     <div className='head'><h1 >What we do</h1></div>
+     <div className='paragraph'>
+     <p className='para2'>Transforming businesses through technology</p>
+     <p className='para3'>At ProCorp, we go beyond helping businesses transform through technology, We help them make a meaningful difference; to their customers, and to the communities they serve. We have a specialised team of experienced teams. Our services include Agile Software Development, Website Development, Devops,UX/UI, Enterprise application development, Mobile application development,Cloud computing, Block chain development, SAP - Fico, SD and ABAP </p>
+     </div>
+     </div>
+     </div>
+     <div className="happy-cilents">
+                        <div>
+                            <p>14 <span className="symbol">+</span></p>
+                            <p>Years Experience</p>
+                        </div>
+                        <div>
+                            <p>100 <span className="symbol">K+</span></p>
+                            <p>Client Satisfaction</p>
+                        </div>
+                        <div>
+                            <p>24 <span className="symbol">h</span></p>
+                            <p>Expert Support Team</p>
+                        </div>
+                        <div>
+                            <p>100 <span className="symbol">K+</span> </p>
+                            <p>Project Delivered</p>
+                        </div>
+                        <div>
+                            <p>100 <span className="symbol">K+</span> </p>
+                             <p>Happy Clients</p>
+                        </div>
+                    </div>
+                    <div >
+                    
+                    <h1 className='miss'>Our Mission</h1>
+                    <div className='mission'>
+                    <p className='para1'>We believe that we're only as good as we do. Being a part of the Service<br/> Sector in India.We aim to be a part of Building th nation by amplifying human <br/>potenital and creating the next opportunity for people, businesses and communities <br/> through our Member, Business, Partner and LGE Portals  </p>
+                    <img src= "https://res.cloudinary.com/dh0odgkdj/image/upload/v1693214324/Image_4_owawts.png" 
+                      alt="" className='laptop'/>
+                    </div>
+                    </div> 
+
+                    <div>
+                    <h1 className='heading'>The story  and values behind <br/>our company</h1>
+                    <p className='value'>Measuring our success by the value we deliver in all directions.</p>
+                    </div>
+                    <div className='container'>
+                    <div className='team-container'>
+                      <div className='team-image-container'>
+                        <img className='team-image' src="https://res.cloudinary.com/dltqytfqi/image/upload/v1693811871/Icon_1_dsvp2k.svg" alt="" />
+                      </div>
+                      <div className='team-details'>
+                        <h1 className='main-head'>Innovation</h1>
+                        <p className='paras'>At ProCorp, innovation is our driving force. We thrive on pushing the boundaries of what's possible in the world of Technology. Our team of visionary thinkers and creative problem solvers constantly seeks out new ways to provide cutting-edge technologies. From envisioning bold ideas to bringing them to life, we're committed to driving innovation that transforms challenges into opportunities.</p>
+                      </div>
+                    </div>
+                   
+                    <div className='team-container'>
+                      <div className='team-image-container'>
+                        <img className='team-image' src="https://res.cloudinary.com/dltqytfqi/image/upload/v1693811871/Icon_1_dsvp2k.svg" alt="" />
+                      </div>
+                      <div className='team-details'>
+                        <h1>team work</h1>
+                        <p className='paras'>Teamwork is the cornerstone of the our success. Our diverse and collaborative team brings together individuals with unique talents, perspectives, and and expertise. We foster environment where every teams member's contribution is valued, and collaboration is not just encouraged but celebrated. By working together seamlessly. we harness the collective power of our teams to overcome challenges and drive impactful results.</p>
+                      </div>
+                    </div>
+                    </div>
+
+            <div className='container1'>
+            <div className='team-container'>
+              <div className='team-image-container'>
+                <img className='team-image' src="https://res.cloudinary.com/dltqytfqi/image/upload/v1693816616/Icon_2_htcqgq.svg" alt="" />
+              </div>
+              <div className='team-details'>
+                <h1>Excellence </h1>
+                <p className='paras'>Excellence is ingrained in everything we do. We believe that every details matters, and our pursuit of excellence is evident in the exceptional quality of our products and services. Our dedication to delivering solutions that exceed expectations has earned us the trust of clients and partners alike. We hold ourselves to the highest standards, ensuring that every project we undertake is testament to our commitment to excellence.</p>
+              </div>
+            </div>
+            <div className='team-container'>
+              <div className='team-image-container'>
+                <img className='team-image' 
+                src="https://res.cloudinary.com/dltqytfqi/image/upload/v1693816219/Icon_1_puregm.png" alt="" />
+              </div>
+              <div className='team-details'>
+                <h1>Responsibility</h1>
+                <p className='paras'>With great innovation comes great responsibility.At
+                ProCorp, we recognize our role in shaping the services we 
+                provide. We take our responsibility seriously - not only to our clients and partners but also to our planet and society.
+                Sustainability and ethical practices are at the forefront of our decision-making.We are committed to making a positive impact on the world through our technology, while ensuring that our actions are guided by principles of social and environmental responsibility.</p>
+              </div>
+            </div>
+            </div>
+            <div>
+            <img src="https://res.cloudinary.com/dltqytfqi/image/upload/v1694062599/Call_to_action_for_About_us_1_fv5uur.png"
+            className='here'/>
+            </div>
+            </div>
+  )
+}
+
+export default index;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import About from './components/About';
 import reportWebVitals from './reportWebVitals';
+
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primereact/resources/primereact.min.css";                                       
+        
+//icons
+import "primeicons/primeicons.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <About/>
   </React.StrictMode>
 );
 
